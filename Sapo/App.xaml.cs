2ca125a5f8adaf749f3c6a989ba818965381f47e
Rayssa <rayssa.cardoso.gonc@gmail>
2024-03-27 09:09:04 -0300

    iniciando

---
+++ Sapo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Sapo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Sapo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Sapo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
